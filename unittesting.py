50e9feef8c8775b97b75fbd6f4fec528bed0b332
Christopher Upkes <cupkes@stratatron.com>
2015-07-15 14:55:34 -0700

    added test cases for macload plugin

--- unittesting.py
+++ unittesting.py
@@ -2,11 +2,12 @@ __author__ = 'christopherupkes'
 
 import nagiosplugin
 from   nagiosplugintest import *
+from   macload import *
 import unittest
 
 class TestNagiosPluginMethods(unittest.TestCase):
 
-  def test_name_check(self):
+  def test_base_name_check(self):
       npt = NagiosPluginTest()
       check_metric = npt.probe()
       result_metric = check_metric[0]
@@ -15,12 +16,25 @@ class TestNagiosPluginMethods(unittest.TestCase):
       self.assertEqual(result_metric.value, True)
 
 
-  def test_value_check(self):
+  def test_base_value_check(self):
       npt = NagiosPluginTest()
       check_metric = npt.probe()
       result_metric = check_metric[0]
       self.assertEqual(result_metric.value, True)
 
+  def test_macload_name_test(self):
+      mlt = MacLoad()
+      result = mlt.probe()
+      self.assertEqual(next(result).name,'load1')
+      self.assertEqual(next(result).name, 'load5')
+      self.assertEqual(next(result).name, 'load15')
+
+  def test_macload_value_test(self):
+      mlt = MacLoad()
+      result = mlt.probe()
+      self.assertGreater(next(result).value,0)
+      self.assertGreater(next(result).value,0)
+      self.assertGreater(next(result).value,0)
 
 
 if __name__ == '__main__':
